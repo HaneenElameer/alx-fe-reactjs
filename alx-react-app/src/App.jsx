7dabe334a2bd02c3b820fe0bfd1cf25281be5b11
Haneen Elameer <Haneenelameer23@gmail.com>
2024-11-13 20:45:53 +0200

    new message

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <WelcomeMessage/>
+        <WelcomeMessage />
       </div>
       <h1>Vite + React</h1>
       <h1>hello world</h1>
