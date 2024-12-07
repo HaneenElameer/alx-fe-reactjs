e2689751d33a017f34923e72577d950267c4c645
Haneen Elameer <Haneenelameer23@gmail.com>
2024-12-07 15:40:12 +0200

    new file

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <footer  style={{ backgroundColor: 'grey', color: 'white', textAlign: 'center' }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+
+
+export default Footer
